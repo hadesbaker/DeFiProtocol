4f5331d41d8cfceca23c00d8262a711a3289c474
Hades Baker <hades@Hades.local>
2023-08-24 11:58:51 -0700

    Fixed deploy.py

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,6 +6,7 @@ def deploy():
     account = get_account()
     token = Token.deploy("HadesCoin", "HC", {"from": account})
     lending_pool = LendingPool.deploy(token.address, {"from": account})
+    lending_pool.wait(1)
 
 
 def main():
